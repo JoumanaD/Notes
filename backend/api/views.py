471eb35cdb7c08153400eacfaf39d176739781ec
21107646 <21107646@etu.sorbonne-universite.fr>
2025-07-24 16:40:38 +0200

    add home & fiw some bugs

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,7 +5,7 @@ from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Note
 
-class NoteListCreate(generics.ListAPIView):
+class NoteListCreate(generics.ListCreateAPIView):
     serializer_class = NoteSerializer
     permission_classes = [IsAuthenticated]
 
